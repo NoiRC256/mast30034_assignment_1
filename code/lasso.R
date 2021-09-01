30e7c5bf1973e00d929e721be163496f58c13e58
NoiR_CCC <noir662607004@gmail.com>
2021-09-01 21:55:34 +1000

    Start LR

---
+++ code/lasso.R
@@ -0,0 +1,32 @@
+nsrcs = 6
+N = 240
+V = 441
+SM_SIZE = 21
+
+x1 = 21
+x2 = 21
+
+# TODO: Import X and TC from main.ipynb;
+
+
+
+# LASSO Regression;
+# From Assignment 1 specs;
+
+step <- 1/(norm(TC %*% t(TC)) * 1.1)
+thr <- rho*N*step
+Ao <- matrix(0, nsrcs, 1)
+A <- matrix(0, nsrcs, 1)
+Alr <- matrix(0, nsrcs, x1*x2)
+
+for (k in 1:(x1*x2)) {
+  A <- Ao+step*(t(TC) %*% (X[,k]-(TC%*%Ao)))
+  A <- (1/(1+thr)) * (sign(A)*pmax(replicate(nsrcs, 0), abs(A)-thr))
+  
+  for (i in 1:10) {
+    Ao <- A
+    A <- Ao+step * (t(TC)%*%(X[,k]-(TC%*%Ao)))
+    A <- (1/(1+thr)) * (sign(A)*pmax(replicate(nsrcs, 0), abs(A)-thr))
+  }
+  Alr[,k] <- A
+}
