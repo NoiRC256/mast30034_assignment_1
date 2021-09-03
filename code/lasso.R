213ec81e16754056290a8546b96444f5ea87065e
NoiR_CCC <noir662607004@gmail.com>
2021-09-03 21:26:04 +1000

    LR realizations

--- code/lasso.R
+++ code/lasso.R
@@ -7,12 +7,20 @@ x1 = 21
 x2 = 21
 
 # TODO: Import X and TC from main.ipynb;
-X <- read.table(file ="X.txt", header=TRUE)
+X <- read.table(file ="X1.txt", header=FALSE, sep=" ")
+colnames(X) <- NULL
+X = as.matrix(X)
 dim(X)
+
+TC <- read.table(file ="TC.txt", header=FALSE, sep=" ")
+colnames(TC) <- NULL
+TC = as.matrix(TC)
+dim(TC)
 
 
 # LASSO Regression;
 # From Assignment 1 specs;
+rho<- seq(from = 0, to = 1, by = 0.05)
 
 step <- 1/(norm(TC %*% t(TC)) * 1.1)
 thr <- rho*N*step
@@ -31,3 +39,5 @@ for (k in 1:(x1*x2)) {
   }
   Alr[,k] <- A
 }
+
+Ao+step*(t(TC) %*% (X[,k]-(TC%*%Ao)))
